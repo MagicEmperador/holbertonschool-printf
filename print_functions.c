e44d56cf8d72f052ae305315d03986bda0ba8db2
MagicEmperador <jose_duarte@outlook.fr>
2023-11-21 02:29:01 -0800

    print functions

--- print_functions.c
+++ print_functions.c
@@ -1,3 +1,18 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+ * p_char - fun of printing char
+ * @list: list of char
+ * Return: return the nr of elements
+ */
+int p_char(va_list list)
+{
+	char c = va_arg(list, int);
+
+	_putchar(c);
+	return (1);
+}
 /**
  * p_str - fun of printing string
  * @list: list of string
@@ -15,21 +30,40 @@ int p_str(va_list list)
 		_putchar(str[i]);
 	return (i);
 }
-
 /**
- * p_str - fun of printing string
- * @list: list of string
- * Return: return the nr of element
+ * p_int_rec - print int digits
+ * @n: number
+ * @count: digits counter
+ * Return: return the digits counter
  */
-int p_str(va_list list)
+int p_int_rec(long int n, int count)
 {
-	int i;
-	char *str;
+	int nr_digits = count;
 
-	str = va_arg(list, char *);
-	if (str == NULL)
-		str = "(null)";
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(str[i]);
-	return (i);
+	if (n < 0)
+	{
+		_putchar('-');
+		n = (-1) * n;
+	}
+	if (n / 10)
+	{
+		nr_digits = p_int_rec(n / 10, count + 1);
+	}
+	_putchar(n % 10 + '0');
+	return (nr_digits);
+}
+/**
+ * p_int - print int
+ * @list: list of elem
+ * Return: return the value
+ *
+ */
+int p_int(va_list list)
+{
+	long int n;
+
+	n = va_arg(list, int);
+	if (n < 0)
+		return (p_int_rec(n, 2));
+	return (p_int_rec(n, 1));
 }
